ba9ec3a4e7c8b136f804feec80c413de5eb0c87c
yuriha-chan <yuriha.tsuchiura@gmail.com>
2023-06-16 03:19:30 +0900

    Remove some shutdown handlers

--- packages/backend/src/server/ServerService.ts
+++ packages/backend/src/server/ServerService.ts
@@ -224,8 +224,8 @@ export class ServerService implements OnApplicationShutdown {
 
 	@bindThis
 	public async dispose(): Promise<void> {
-    await this.streamingApiServerService.detach();
-		await this.#fastify.close();
+		// await this.streamingApiServerService.detach();
+		// await this.#fastify.close();
 	}
 
 	@bindThis
