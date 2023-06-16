1392a0416a6e013ceae343142949788578fdecb6
Yuriha <yuriha.tsuchiura@gmail.com>
2023-06-16 09:13:23 +0000

    Users can report themselves and admins

--- packages/backend/src/server/api/endpoints/users/report-abuse.ts
+++ packages/backend/src/server/api/endpoints/users/report-abuse.ts
@@ -25,17 +25,6 @@ export const meta = {
 			id: '1acefcb5-0959-43fd-9685-b48305736cb5',
 		},
 
-		cannotReportYourself: {
-			message: 'Cannot report yourself.',
-			code: 'CANNOT_REPORT_YOURSELF',
-			id: '1e13149e-b1e8-43cf-902e-c01dbfcb202f',
-		},
-
-		cannotReportAdmin: {
-			message: 'Cannot report the admin.',
-			code: 'CANNOT_REPORT_THE_ADMIN',
-			id: '35e166f5-05fb-4f87-a2d5-adb42676d48f',
-		},
 	},
 } as const;
 
@@ -71,14 +60,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				if (err.id === '15348ddd-432d-49c2-8a5a-8069753becff') throw new ApiError(meta.errors.noSuchUser);
 				throw err;
 			});
-
-			if (user.id === me.id) {
-				throw new ApiError(meta.errors.cannotReportYourself);
-			}
-
-			if (await this.roleService.isAdministrator(user)) {
-				throw new ApiError(meta.errors.cannotReportAdmin);
-			}
 
 			const report = await this.abuseUserReportsRepository.insert({
 				id: this.idService.genId(),
