acf7b6068246e0e08c83951f80d234354572ecfa
Yuriha <yuriha.tsuchiura@gmail.com>
2023-06-16 19:13:56 +0000

    Revert home timeline age limit

--- packages/backend/src/server/api/endpoints/notes/timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/timeline.ts
@@ -65,7 +65,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			//#region Construct query
 			const query = this.queryService.makePaginationQuery(this.notesRepository.createQueryBuilder('note'),
 				ps.sinceId, ps.untilId, ps.sinceDate, ps.untilDate)
-				.andWhere('note.id > :minId', { minId: this.idService.genId(new Date(Date.now() - (1000 * 60 * 60 * 24 * 10))) }) // 10日前まで
+				.andWhere('note.id > :minId', { minId: this.idService.genId(new Date(Date.now() - (1000 * 60 * 60 * 24 * 60))) }) // 60日前まで
 				.innerJoinAndSelect('note.user', 'user')
 				.leftJoinAndSelect('note.reply', 'reply')
 				.leftJoinAndSelect('note.renote', 'renote')
