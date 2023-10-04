88016884e72180ff84fa15e0b65d21e3e1b32d6b
yuriha-chan <yuriha.tsuchiura@gmail.com>
2023-10-05 00:19:57 +0900

    [frontend] Revert default reaction size to small

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -323,7 +323,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	reactionsDisplaySize: {
 		where: 'device',
-		default: 'medium' as 'small' | 'medium' | 'large',
+		default: 'small' as 'small' | 'medium' | 'large',
 	},
 	forceShowAds: {
 		where: 'device',
