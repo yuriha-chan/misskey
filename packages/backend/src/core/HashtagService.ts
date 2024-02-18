23a3b3db869da3ec847e8e91f2c4ad3d7a898f6a
Yuriha <yuriha.tsuchiura@gmail.com>
2024-02-18 13:44:01 +0000

    Add scriptable keyword filters

--- packages/backend/src/core/HashtagService.ts
+++ packages/backend/src/core/HashtagService.ts
@@ -163,7 +163,7 @@ export class HashtagService {
 		const instance = await this.metaService.fetch();
 		const hiddenTags = instance.hiddenTags.map(t => normalizeForSearch(t));
 		if (hiddenTags.includes(hashtag)) return;
-		if (this.utilityService.isKeyWordIncluded(hashtag, instance.sensitiveWords)) return;
+		if (this.utilityService.isKeyWordIncluded(instance.sensitiveWords, hashtag)) return;
 
 		// YYYYMMDDHHmm (10分間隔)
 		const now = new Date();
