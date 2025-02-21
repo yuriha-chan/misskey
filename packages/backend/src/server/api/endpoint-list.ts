0f0d0acfdf7e46ee2370b6dc2c3fd27cd5b818d8
Yuriha <yuriha.tsuchiura@gmail.com>
2025-02-21 17:05:26 +0000

    fix merge: api

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -303,6 +303,7 @@ export * as 'notes/delete' from './endpoints/notes/delete.js';
 export * as 'notes/favorites/create' from './endpoints/notes/favorites/create.js';
 export * as 'notes/favorites/delete' from './endpoints/notes/favorites/delete.js';
 export * as 'notes/featured' from './endpoints/notes/featured.js';
+export * as 'notes/followings-updates' from './endpoints/notes/followings-updates.js';
 export * as 'notes/global-timeline' from './endpoints/notes/global-timeline.js';
 export * as 'notes/hybrid-timeline' from './endpoints/notes/hybrid-timeline.js';
 export * as 'notes/local-timeline' from './endpoints/notes/local-timeline.js';
