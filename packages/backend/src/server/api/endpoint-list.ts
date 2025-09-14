0d5b2367e93d390e105f68ad0dc83ecb718fee95
Ubuntu <yuriha.tsuchiura@gmail.com>
2025-09-14 07:11:04 +0000

    fix update-visibility

--- packages/backend/src/server/api/endpoint-list.ts
+++ packages/backend/src/server/api/endpoint-list.ts
@@ -340,6 +340,7 @@ export * as 'notes/thread-muting/delete' from './endpoints/notes/thread-muting/d
 export * as 'notes/timeline' from './endpoints/notes/timeline.js';
 export * as 'notes/translate' from './endpoints/notes/translate.js';
 export * as 'notes/unrenote' from './endpoints/notes/unrenote.js';
+export * as 'notes/update-visibility' from './endpoints/notes/update-visibility.js';
 export * as 'notes/user-list-timeline' from './endpoints/notes/user-list-timeline.js';
 export * as 'notifications/create' from './endpoints/notifications/create.js';
 export * as 'notifications/flush' from './endpoints/notifications/flush.js';
