7e84bdd1db27f2df05d4aa6c0f08438c0203ba68
Yuriha <yuriha.tsuchiura@gmail.com>
2026-06-02 16:13:21 +0000

    fix merge

--- packages/frontend/src/types/page-header.ts
+++ packages/frontend/src/types/page-header.ts
@@ -7,8 +7,6 @@ export type PageHeaderItem = {
 	text: string;
 	icon: string;
 	highlighted?: boolean;
-	danger?: boolean;
-	showText?: boolean;
 	handler: (ev: PointerEvent) => void;
 	showText?: boolean;
 	danger?: boolean;
