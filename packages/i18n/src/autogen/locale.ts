2e8732ff95ca38ad9aa52a5736f09c093725ce97
Yuriha <yuriha.tsuchiura@gmail.com>
2026-06-04 09:19:41 +0000

    chore: autogen

--- packages/i18n/src/autogen/locale.ts
+++ packages/i18n/src/autogen/locale.ts
@@ -7149,11 +7149,11 @@ export interface Locale extends ILocale {
          */
         "laterAreYouSure": string;
         /**
-         * 絵文字パレット設定
+         * 絵文字パレット初期設定
          */
         "emojiPaletteSetting": string;
         /**
-         * よく使う絵文字のパレットを選んでください。パレットは後から設定画面でカスタマイズできます。
+         * 投稿への反応に使うスタンプ (絵文字) のパレットを選択できます。どのパレットを選んでも、後から自由に追加・削除できます。
          */
         "emojiPaletteSettingDescription": string;
         /**
@@ -7161,7 +7161,7 @@ export interface Locale extends ILocale {
          */
         "useThisPalette": string;
         /**
-         * おすすめのパレットはありません。デフォルトのパレットが設定されています。
+         * おすすめのパレットはありません。デフォルトのパレットが設定されます。
          */
         "noRecommendedPalettes": string;
     };
